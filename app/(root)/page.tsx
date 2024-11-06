e995e3cbd1cc26df4e4c63199b5768505e58a7c7
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-11-05 20:13:21 -0600

    All startups and startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -7,6 +8,20 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Randall" },
+      _id: 1,
+      description:
+        "This is a tool that allows you to generate your own custom thumbnails for YouTube videos.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs6gtfUw4MOCO3NsHKqKdQbfzzieU34AK80w&s",
+      category: "Content Creators",
+      title: "Thumbnails",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -18,6 +33,20 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
