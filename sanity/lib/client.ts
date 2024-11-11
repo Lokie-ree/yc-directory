7e089678bc1ddaaff9741823c121e5baa4f2f8de
Lokie-ree <165349722+Lokie-ree@users.noreply.github.com>
2024-11-11 11:36:22 -0600

    Author Authentication Flow

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,16 +1,9 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
 import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true,
 });
-
-const builder = imageUrlBuilder(client);
-
-export function urlFor(source: any) {
-  return builder.image(source);
-}
